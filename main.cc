e06ab78b457d25c3b4a8d8934b59c5cd1d5ea38e
Oliver Lee <oliverzlee@gmail.com>
2021-02-07 00:11:05 +0100

    Replace `integrate` with a const step range

--- main.cc
+++ main.cc
@@ -8,27 +8,36 @@
 
 namespace {
 
-using Model = dyn::model<double, std::ratio<1105, 1000>, std::ratio<1738, 1000>>;
+template <class Iterator>
+auto adapt_rangepair(std::pair<Iterator, Iterator> rp)
+{
+    struct range {
+        decltype(rp.first) begin_;
+        decltype(rp.second) end_;
 
-auto states = std::vector<std::pair<double, Model::state>>{};
+        auto begin() { return begin_; }
+        auto end() { return end_; }
+    };
 
-struct Observer {
-    void operator()(const Model::state& x, double t) { states.emplace_back(t, x); }
-};
+    return range{rp.first, rp.second};
+}
 
 }  // namespace
 
 int main()
 {
-    const auto f = Model::state_transition(Model::input{0.0, 0.2});
-
-    auto x = Model::state{0.0, 0.0, 0.0, 10.0};
-    boost::numeric::odeint::integrate(f, x, 0.0, 10.0, 0.1, Observer{});
+    namespace odeint = boost::numeric::odeint;
+    using Model = dyn::model<double, std::ratio<1105, 1000>, std::ratio<1738, 1000>>;
 
     std::cout << std::left << std::setprecision(3) << std::fixed;
-
     std::cout << Model{} << std::endl;
-    for (const auto& s : states) {
-        std::cout << "t=" << s.first << ": " << s.second << '\n';
+
+    auto stepper = odeint::runge_kutta4<Model::state>{};
+    const auto f = Model::state_transition(Model::input{0.0, 0.2});
+    auto x = Model::state{0.0, 0.0, 0.0, 10.0};
+
+    for (const auto& s :
+         adapt_rangepair(odeint::make_const_step_time_range(stepper, f, x, 0.0, 10.0, 0.1))) {
+        std::cout << "t=" << s.second << ": " << s.first << '\n';
     }
 }
