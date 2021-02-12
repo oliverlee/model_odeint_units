1dc0705cc5278c5e33408a5f13b8fdfb68935b1d
Oliver Lee <oliverzlee@gmail.com>
2021-02-12 23:24:29 +0100

    Calculate derivative at compile time

--- main2.cc
+++ main2.cc
@@ -1,39 +1,96 @@
+#include "gcem.hpp"
 #include "state_space/vector.h"
 #include "units.h"
 
 #include <chrono>
 #include <iostream>
-#include <tuple>
+
+namespace {
+
+namespace math {
+
+template <class AngleUnit>
+constexpr auto sin(const AngleUnit angle) noexcept
+{
+    static_assert(units::traits::is_angle_unit<AngleUnit>::value, "");
+    return units::dimensionless::scalar_t(
+        gcem::sin(angle.template convert<units::angle::radian>().value()));
+}
+
+template <class AngleUnit>
+constexpr auto cos(const AngleUnit angle) noexcept
+{
+    static_assert(units::traits::is_angle_unit<AngleUnit>::value, "");
+    return units::dimensionless::scalar_t(
+        gcem::cos(angle.template convert<units::angle::radian>().value()));
+}
+
+template <class AngleUnit>
+constexpr auto tan(const AngleUnit angle) noexcept
+{
+    static_assert(units::traits::is_angle_unit<AngleUnit>::value, "");
+    return units::dimensionless::scalar_t(
+        gcem::tan(angle.template convert<units::angle::radian>().value()));
+}
+
+template <class ScalarUnit>
+constexpr auto atan(const ScalarUnit x) noexcept
+{
+    static_assert(units::traits::is_dimensionless_unit<ScalarUnit>::value, "");
+    return units::angle::radian_t(gcem::atan(x.value()));
+}
+
+}  // namespace math
+
+using namespace units::literals;
+using namespace std::literals::chrono_literals;
+
+constexpr auto lf = 1.105_m;
+constexpr auto lr = 1.738_m;
+
+constexpr auto beta = math::atan(lr / (lf + lr) * math::tan(0.2_rad));
+
+using state = dyn::state_space::vector<struct x,
+                                       units::length::meter_t,
+                                       struct y,
+                                       units::length::meter_t,
+                                       struct yaw,
+                                       units::angle::radian_t,
+                                       struct v,
+                                       units::velocity::meters_per_second_t>;
+
+using deriv = state::derivative<1>;
+
+struct f {
+    constexpr auto operator()(const state& sx, deriv& dxdt, typename state::duration_type) -> void
+    {
+        dxdt.get<x>() = sx.get<v>() * math::cos(sx.get<yaw>() + beta);
+        dxdt.get<y>() = sx.get<v>() * math::sin(sx.get<yaw>() + beta);
+        dxdt.get<yaw>() = sx.get<v>() / lr * math::sin(beta) * 1_rad;
+        dxdt.get<v>() = 0_mps_sq;
+    }
+};
+
+constexpr auto calc_deriv(const state& x) -> deriv
+{
+    auto dxdt = deriv{};
+
+    f{}(x, dxdt, 0s);
+
+    return dxdt;
+}
+
+}  // namespace
 
 int main()
 {
-    using namespace units::literals;
+    constexpr auto dxdt = calc_deriv({0_m, 0_m, 0_rad, 10_mps});
 
-    using state = dyn::state_space::
-        vector<struct x, units::length::meter_t, struct v, units::velocity::meters_per_second_t>;
-
-    {
-        constexpr auto s = state{1_m, 2_mps} * 2;
-
-        std::cout << "state: " << std::endl;
-        std::cout << "x: " << s.get<x>() << std::endl;
-        std::cout << "v: " << s.get<v>() << std::endl;
-    }
-
-
-    {
-        constexpr auto d =
-            state::derivative<1>{1_mps, 2_mps_sq} + state::derivative<1>{4_mps, 3_mps_sq};
-
-        std::cout << "derivative: " << std::endl;
-        std::cout << "x: " << d.get<x>() << std::endl;
-        std::cout << "v: " << d.get<v>() << std::endl;
-
-        constexpr auto i = d * std::chrono::seconds{1};
-        std::cout << "integral of derivative: " << std::endl;
-        std::cout << "x: " << i.get<x>() << std::endl;
-        std::cout << "v: " << i.get<v>() << std::endl;
-    }
+    std::cout << "deriv: " << std::endl;
+    std::cout << "x: " << dxdt.get<x>() << std::endl;
+    std::cout << "y: " << dxdt.get<y>() << std::endl;
+    std::cout << "yaw: " << dxdt.get<yaw>() << std::endl;
+    std::cout << "v: " << dxdt.get<v>() << std::endl;
 
     return 0;
 }
